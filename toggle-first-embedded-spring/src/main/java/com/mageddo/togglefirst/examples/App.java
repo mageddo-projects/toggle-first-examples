6ee3ea1c28e8140fce0fc036e719f727a57f9992
Elvis de Freitas <edigitalb@gmail.com>
2019-04-19 12:08:24 -0300

    unnecessary annotation

--- toggle-first-embedded-spring/src/main/java/com/mageddo/togglefirst/examples/App.java
+++ toggle-first-embedded-spring/src/main/java/com/mageddo/togglefirst/examples/App.java
@@ -11,10 +11,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableToggleFirst
 public class App {
 
